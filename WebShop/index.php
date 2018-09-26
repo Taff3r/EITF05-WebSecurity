e7fcf0e189ef932cdbcad82c0b8162b0c8d0d46b
Simon Tenggren <simon.tenggren@gmail.com>
2018-09-26 14:35:36 +0200

    lite mer divs

--- WebShop/index.php
+++ WebShop/index.php
@@ -4,20 +4,31 @@
 	</style>
 
 	<title>
-		Login/Register
+		Welcome to eXoticPets.com
 	</title>
 	<head></head>
 	<body>
-		<div>
-			<p> This is paragraph yes </p>
-			<form action = "login.php" method = "post">
-			<input type = "submit" value = "I want to log in">
-			</form>
-
-			<form action = "register.php" method = "post">
-			<input type = "submit" value = "I want to register">
-			</form>
+		<div class = "header">
+			<div class = "alignButton">
+				<form action = "login.php" method = "post" class = "button">
+					<input type = "submit" value = "I want to log in">
+				</form>
+				<form action = "register.php" method = "post" class = "button">
+					<input type = "submit" value = "I want to register">
+				</form>
+				<form action ="checkout.php" method="get" class = "button">
+					<input type = "submit" value = "Checkout">
+				</form>
+			</div>
+			<div class = "headerText">
+				<h1> eXoticPets.com <p class ="italicsHeader"> - We're totally legal </p></h1>
+			</div>
 		</div>
+
+		<div class= "sideMenu">
+			<p> test </p>
+		</div>
+
 	</body>
 
 </html>

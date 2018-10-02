2c5c2c20f99c3a26d7ba847c52156ea3a0612667
Nicoline Kjaer helgstrand <nicolinekjaerhelgstrand@Nicolines-MacBook-Air.local>
2018-10-02 17:31:02 +0200

    Bilder och add-knapp till browseprodukts

---
+++ WebShop/products.php
@@ -0,0 +1,52 @@
+<html>
+<style> <?php include 'css/index.css'; ?>
+</style>
+<head>
+<title> Browse Products </title>
+<link rel="stylessheet" type="text/css" href="css/index.css">
+</head>
+<body>
+
+<h1> Products </h1>
+
+<div class ="row">
+
+   
+<img src='bird.jpg' alt ="Bird" width="150" height="200"/>
+<p> Macaw parrot from South America </p>
+<form  method="post" action="products.php">
+    
+    <input type="submit" value = "Add to cart">
+  </form>
+
+<img src="giraffe.jpg" alt ="Giraffe"  width="150" height="200"/>
+<p> Giraff </p>
+<form  method="post" action="products.php">
+   
+    <input type="submit" value = "Add to cart">
+  </form>
+
+<img src="tigers.jpg" alt ="Giraffe" width="150" height="200"/>
+<p> Beautiful tiger from Nepal </p>
+<form  method="post" action="products.php">
+    
+    <input type="submit" value = "Add to cart">
+  </form>
+
+<img src="lemur.jpg" alt ="Lemur"  width="150" height="200"/>
+<p> Lemur from Ystaddjurpark </p>
+<form  method="post" action="products.php">
+ 
+    <input type="submit" value = "Add to cart">
+  </form>
+
+<img src="lions.jpg" alt ="lion" width="150" height="200"/>
+<p> White lion </p>
+<form  method="post" action="products.php">
+
+    <input type="submit" value = "Add to cart">
+  </form>
+
+</div>
+</body>
+</html>

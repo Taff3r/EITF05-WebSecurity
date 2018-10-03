2a5c4b99da90823bf1d58e1bb94e8737ab3631bb
Nicoline Kjaer helgstrand <nicolinekjaerhelgstrand@Nicolines-MacBook-Air.local>
2018-10-03 10:17:21 +0200

    lagt till så vår checkout funkar, (dock ej kvitto än) obs namna på sql rader i databasen

--- WebShop/checkout.php
+++ WebShop/checkout.php
@@ -1,6 +1,48 @@
-<?php>
+<html>
+<style> <?php include 'css/index.css'; ?>
+</style>
+<head>
+<title> Checkout </title>
+<link rel="stylessheet" type="text/css" href="css/index.css">
+</head>
+<body>
 
-Welcome to the checkout
+<h1> Cart </h1>
+<div class ="row">
 
+<a href="index.php">Return home</a>
+<br>
+<?php 
+if(isLoggedIn()){
+	$user = $_COOKIE['username'];
+	$conn = new mysqli("localhost", "root", "","WebShopDB");
+	#har döpt product_name men vet ej vad den heter i er WebShopDB
+	$getQuery = "SELECT product_name FROM itemsincart WHERE username = '$user'";
+	
+		if($result = mysqli_query($conn, $getQuery)){
+			
+			while($row = mysqli_fetch_assoc($result)){
+				#printf("(%s \n )", $row["product_name"]);
+				echo( $row["product_name"]);
+				echo "<br>";
+			}
+			 }else{
+			  echo "0 products in cart";
+			  }
+}else{
+echo "You are not logged in and therfore have no items in your cart";
+}
 
-</php>
+function isLoggedIn(){
+  return isset($_COOKIE['username']);
+}
+?>
+
+<form  method="post" action="checkout.php">
+
+    <input type="submit" name = "" value = "Buy">
+  </form>
+
+</div>
+</body>
+</html>

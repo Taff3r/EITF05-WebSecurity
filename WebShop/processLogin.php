e9cb31c5ee51dadb8276f3902ee0c69860ffb536
williamrosenberg <william.rosenberg@live.se>
2018-10-04 11:25:36 +0200

    Ändrade attemts-queryn i processLogin så att varningen ignoreras när queryn inte returnerar någonting.

--- WebShop/processLogin.php
+++ WebShop/processLogin.php
@@ -4,7 +4,7 @@
 	$conn = new mysqli("localhost", "root", "","WebShopDB"); # or die("Connect failed: %s\n". $conn -> error);
 
 	$lookup = mysqli_fetch_array(mysqli_query($conn,"SELECT * FROM users WHERE name = '$username'"));
-  $attempts = mysqli_fetch_array(mysqli_query($conn,"SELECT attempts FROM loginAttempts WHERE name = '$username'"));
+  $attempts = @mysqli_fetch_array(mysqli_query($conn,"SELECT attempts FROM loginAttempts WHERE name = '$username'"));
     if($attempts['attempts'] >= 5){
       echo "Too many attempts to login for this user was made, please contact the admin at admin@forget.it";
       echo ' <a href="index.php">return home</a> ';

